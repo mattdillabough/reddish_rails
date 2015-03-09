9880f4449d790c5f881a43d1ed6a605461f4a5c6
Travis Briggs <audiodude@gmail.com>
2015-03-09 04:16:00 +0000

    User table migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223052645) do
+ActiveRecord::Schema.define(version: 20150309040138) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150223052645) do
     t.integer  "user_id",     limit: 4
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "username",   limit: 255
+    t.string   "password",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
