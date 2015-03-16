6245f718a5cbfa3115c0ba03345712352eab2518
Travis Briggs <audiodude@gmail.com>
2015-03-15 21:44:15 -0700

    Initial basics for creating a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    User.create(user_params)
+    redirect_to root_path
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
