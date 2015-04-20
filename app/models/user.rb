48e2481ff99444863f69050770430f9df766b086
Travis Briggs <audiodude@gmail.com>
2015-04-19 21:21:21 -0700

    Username requirements.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+  RE_USERNAME = /\A[A-Za-z0-9_]+\z/
+  
   has_many :links
   validates :email, presence: true
-  validates :username, presence: true
+  validates :username, presence: true, length: {minimum: 4, maximum: 20}, format: {with: RE_USERNAME, message: "must contain only letters, digits and underscore"}
   has_secure_password
 end
