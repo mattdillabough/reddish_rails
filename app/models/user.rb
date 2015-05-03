51130fbf78e61cea8e2145a1a1c5a0a6a5ce06fc
Matt Dillabough <mattdillabough@gmail.com>
2015-05-03 13:53:35 -0700

    Unique email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   RE_EMAIL = /\A[A-Za-z0-9_.]+@[A-Za-z0-9_.]+\z/
   
   has_many :links
-  validates :email, presence: true, format: {with: RE_EMAIL, message: "must be a valid email"}
+  validates :email, presence: true, uniqueness:true, format: {with: RE_EMAIL, message: "must be a valid email"}
   validates :username, presence: true, uniqueness: true, length: {minimum: 4, maximum: 20}, format: {with: RE_USERNAME, message: "must contain only letters, digits and underscore"}
   has_secure_password
 end
