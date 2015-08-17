a2f79d1aeeaacfa6613a1375c44bd6c52eff3cc5
Travis Briggs <audiodude@gmail.com>
2015-08-17 04:05:18 +0000

    Use find_by_id in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def get_current_user
-  	@current_user = User.find(session[:user_id]) if session[:user_id]
+    @current_user = User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
 end
