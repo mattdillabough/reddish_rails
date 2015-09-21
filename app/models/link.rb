0abf18c644743a578aa927ba60e594cfe7c9e143
Travis Briggs <audiodude@gmail.com>
2015-09-21 04:13:30 +0000

    Link title validation.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,9 +1,10 @@
 class Link < ActiveRecord::Base
-  RE_URL = /\Ahttps?:\/\/.*\..*/
+  RE_URL = /\Ahttps?:\/\//
 
   # The links table in the database has a user_id column which records
   # the id of the user who "owns"/created the link.
   belongs_to :user
   
   validates :url, presence: true, format: {with: RE_URL, message: "must start with http(s)://"}
+  validates :title, presence: true
 end
