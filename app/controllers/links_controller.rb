ad326ae03fb5633dac37db6b244fc29d1eef5ca9
Travis Briggs <audiodude@gmail.com>
2015-09-21 04:11:14 +0000

    Mainly restyling pages with bootstrap.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,32 @@
 class LinksController < ApplicationController
+  before_action :redirect_if_not_logged_in, only: ['new', 'create']
+  
   def new
-    
+    @link = Link.new
   end
   
   def create
-    
+    # The 'create' method is essentially 'new and save if valid'
+    @link = Link.create(link_params)
+    if @link.valid?
+      @link.user = get_current_user
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+  
+  private
+  
+  def redirect_if_not_logged_in
+    if !get_current_user
+      @error = 'You must be logged in to create a link.'
+      redirect_to url_for(controller: 'users', action: 'login')
+    end
+  end
+  
+  def link_params
+    # {link: {url: 'http://foo.fake', title: 'Foo', 'dynamite': 'boom'}}
+    params.require(:link).permit(:url, :title, :description) # 4
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,16 @@ class User < ActiveRecord::Base
   RE_USERNAME = /\A[A-Za-z0-9_.]+\z/
   RE_EMAIL = /\A[A-Za-z0-9_.]+@[A-Za-z0-9_.]+\z/
   
+  # This association declaration is the inverse of the "belongs_to" on the link.rb
+  # It indicates that a given object of type User, let's call him user_a, has a collection
+  # of links. So:
+  #
+  # user_a.links
+  #
+  # Is valid code. There are lots of methods that get added to the User class by creating
+  # a has_many association. Search Google for "Rails has_many methods".
   has_many :links
+  
   validates :email, presence: true, uniqueness:true, format: {with: RE_EMAIL, message: "must be a valid email"}
   validates :username, presence: true, uniqueness: true, length: {minimum: 4, maximum: 20}, format: {with: RE_USERNAME, message: "must contain only letters, digits and underscore"}
   has_secure_password
